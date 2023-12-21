14b34c0851f16c13c44c4d539ddf5644d321bca6
Fecony <minepuff@gmail.com>
2023-12-21 23:22:55 +0200

    fix: ignore builtin module build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import { builtinModules } from 'module';
 import { defineConfig } from '@solidjs/start/config';
 
 export default defineConfig({
   start: {
     server: {
       preset: 'bun',
+    },
+  },
+  build: {
+    commonjsOptions: {
+      ignore: [...builtinModules, 'ws'],
     },
   },
 });
